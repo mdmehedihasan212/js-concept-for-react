3c1f9b1b730fde1b2e67f5bba2db55f4a6740440
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-08 21:56:13 +0600

    add function

--- function.js
+++ function.js
@@ -4,3 +4,15 @@ function add(num1, num2) {
 }
 const total = add(10, 5);
 console.log(total);
+
+function bringSingara(taka) {
+    console.log('kolijar singara onek mojader', taka);
+    const singaraPrice = 10;
+    const singaraQuantity = money / singaraPrice;
+    return singaraQuantity;
+
+}
+const money = 250;
+const singara = bringSingara(money);
+console.log('Ai nen singara', singara);
+
