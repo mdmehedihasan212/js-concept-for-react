46c4c6b35bb1657db27d782ffea0205b5ac55a35
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-08 15:14:35 +0600

    add array

---
+++ array.js
@@ -0,0 +1,12 @@
+const numbers = [89, 35, 98, 12];
+const books = ['hobluder jonno programming', 'programming balod to boss', 'programmiger choddhogusthi']
+
+// elem push
+const booksPush = books.push('hate kalome javaScript');
+console.log(books);
+// elem length
+const booksLen = books.length;
+console.log(booksLen);
+// elem index
+const booksIndex = books[2];
+console.log(booksIndex);
